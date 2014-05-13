e2679ed59a3098b9659d72504554998b3e83b566
Carranza <oz.carranza@gmail.com>
2014-05-13 04:27:30 +0200

    Funcionalidad sobre Google Maps.

--- app/src/main/java/com/example/ejemplogooglemaps/MainActivity.java
+++ app/src/main/java/com/example/ejemplogooglemaps/MainActivity.java
@@ -2,34 +2,64 @@ package com.example.ejemplogooglemaps;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends FragmentActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements GoogleMap.OnMapClickListener {
+
+    private final LatLng UGR = new LatLng(37.1809462, -3.5922033);
+    private GoogleMap mapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapa = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(UGR, 15));
+        mapa.setMyLocationEnabled(true);
+        mapa.getUiSettings().setZoomControlsEnabled(false);
+        mapa.getUiSettings().setCompassEnabled(true);
+        mapa.addMarker(new MarkerOptions()
+                .position(UGR)
+                .title("UGR")
+                .snippet("Universidad de Granada: ETSIIT")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher))
+                .anchor(0.5f, 0.5f));
+
+        mapa.setOnMapClickListener(this);
     }
 
+    public void moveCamera(View view) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(UGR));
+    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void animateCamera(View view) {
+        if (mapa.getMyLocation() != null)
+            mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(mapa.getMyLocation().getLatitude(),
+                           mapa.getMyLocation().getLongitude()),
+                           15));
+    }
+
+    public void addMarker(View view) {
+        mapa.addMarker(new MarkerOptions().position(
+            new LatLng(mapa.getCameraPosition().target.latitude,
+                       mapa.getCameraPosition().target.longitude)
+        ));
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onMapClick(LatLng puntoPulsado) {
+        mapa.addMarker(new MarkerOptions().position(puntoPulsado). icon(BitmapDescriptorFactory
+            .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
     }
 }
